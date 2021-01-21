c7692c29da45c4ddb277c67db93831675eff17eb
Jeremy Fergason <jeremy.fergason@corwynn.com>
2021-01-20 22:40:25 -0700

    Add download for FRED

---
+++ data/fred.go
@@ -0,0 +1,73 @@
+package data
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math"
+	"net/http"
+	"strconv"
+	"time"
+
+	dataframe "github.com/rocketlaunchr/dataframe-go"
+	imports "github.com/rocketlaunchr/dataframe-go/imports"
+)
+
+var fredURL = "https://fred.stlouisfed.org"
+
+type fred struct{}
+
+// NewFred Create a new Fred data provider
+func NewFred() fred {
+	return fred{}
+}
+
+// Interface functions
+
+func (f fred) DataType() string {
+	return "rate"
+}
+
+func (f fred) GetDataForPeriod(symbol string, frequency string,
+	begin time.Time, end time.Time) (data *dataframe.DataFrame, err error) {
+	// build URL to get data
+	url := fmt.Sprintf("%s/graph/fredgraph.csv?mode=fred&id=%s&cosd=%s&coed=%s&fq=%s&fam=avg", fredURL, symbol, begin.Format("2006-01-02"), end.Format("2006-01-02"), frequency)
+	log.Printf("Download from FRED: %s\n", url)
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := imports.LoadFromCSV(context.TODO(), bytes.NewReader(body), imports.CSVLoadOptions{
+		DictateDataType: map[string]interface{}{
+			"DATE": imports.Converter{
+				ConcreteType: time.Time{},
+				ConverterFunc: func(in interface{}) (interface{}, error) {
+					return time.Parse("2006-01-02", in.(string))
+				},
+			},
+			symbol: imports.Converter{
+				ConcreteType: float64(0),
+				ConverterFunc: func(in interface{}) (interface{}, error) {
+					v, err := strconv.ParseFloat(in.(string), 64)
+					if err != nil {
+						return math.NaN(), nil
+					}
+					return v, nil
+				},
+			},
+		},
+	})
+
+	return res, err
+}
