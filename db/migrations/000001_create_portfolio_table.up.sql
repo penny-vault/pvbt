e8fd955503d20e3ded77c357684997568cbbf0c2
Jeremy Fergason <jeremy.fergason@corwynn.com>
2021-02-03 23:29:10 -0700

    Update 000001 portfolio structure

--- db/migrations/000001_create_portfolio_table.up.sql
+++ db/migrations/000001_create_portfolio_table.up.sql
@@ -1,6 +1,15 @@
 -- Create portfolio tables that stores saved portfolios
 BEGIN;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.lastchanged = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE TABLE IF NOT EXISTS portfolio (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     userid VARCHAR(32) NOT NULL,
@@ -10,7 +19,15 @@ CREATE TABLE IF NOT EXISTS portfolio (
     ytd_return FLOAT,
     cagr_since_inception FLOAT,
     credentials JSONB NOT NULL,
-    notifications INT NOT NULL DEFAULT 0
+    notifications INT NOT NULL DEFAULT 0,
+    created TIMESTAMP NOT NULL DEFAULT now(),
+    lastchanged TIMESTAMP NOT NULL DEFAULT now()
 );
 CREATE INDEX portfolio_userid_idx ON portfolio(userid);
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON portfolio
+FOR EACH ROW
+EXECUTE FUNCTION trigger_set_timestamp();
+
 COMMIT;
