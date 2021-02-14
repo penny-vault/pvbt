bd31e80fd872eb797105370c4d14c7db34ec0095
Jeremy Fergason <jeremy.fergason@corwynn.com>
2021-02-13 22:23:28 -0700

    Fix strategies adm_test to reflect risk free rate changes

--- strategies/adm_test.go
+++ strategies/adm_test.go
@@ -88,6 +88,15 @@ var _ = Describe("Adm", func() {
 
 		httpmock.RegisterResponder("GET", "https://api.tiingo.com/tiingo/daily/PRIDX/prices?startDate=1989-07-31&endDate=2021-01-01&format=csv&resampleFreq=Monthly&token=TEST",
 			httpmock.NewBytesResponder(200, content))
+
+		content, err = ioutil.ReadFile("testdata/riskfree.csv")
+		if err != nil {
+			panic(err)
+		}
+		httpmock.RegisterResponder("GET", "https://fred.stlouisfed.org/graph/fredgraph.csv?mode=fred&id=DTB3&cosd=1970-01-01&coed=2021-02-13&fq=Daily&fam=avg",
+			httpmock.NewBytesResponder(200, content))
+
+		data.InitializeDataManager()
 	})
 
 	Describe("Compute momentum scores", func() {
@@ -114,30 +123,35 @@ var _ = Describe("Adm", func() {
 					Time:          617846400,
 					Value:         10000,
 					Holdings:      "VFINX",
+					RiskFreeValue: 10000,
 					PercentReturn: 0,
 				}))
 				Expect(perf.Measurement[100]).To(Equal(portfolio.PerformanceMeasurement{
 					Time:          880675200,
 					Value:         42408.6029810143,
 					Holdings:      "VFINX",
+					RiskFreeValue: 15173.981586783602,
 					PercentReturn: 0.045985060690943325,
 				}))
 				Expect(perf.Measurement[200]).To(Equal(portfolio.PerformanceMeasurement{
 					Time:          1143763200,
 					Value:         343579.7507494431,
 					Holdings:      "PRIDX",
+					RiskFreeValue: 19938.252280594555,
 					PercentReturn: 0.06929347826087029,
 				}))
 				Expect(perf.Measurement[300]).To(Equal(portfolio.PerformanceMeasurement{
 					Time:          1406764800,
 					Value:         1.1502482646161714e+06,
 					Holdings:      "VFINX",
+					RiskFreeValue: 21966.917804088513,
 					PercentReturn: -0.01388044019244239,
 				}))
 				Expect(perf.Measurement[378]).To(Equal(portfolio.PerformanceMeasurement{
 					Time:          1611878400,
 					Value:         3.279045827906852e+06,
 					Holdings:      "PRIDX",
+					RiskFreeValue: 23244.597277008164,
 					PercentReturn: 0.027872645147074993,
 				}))
 			})
