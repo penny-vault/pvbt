22ca6886495fcc45cac27c09ad8f7a3a62f8995d
Jeremy Fergason <jeremy.fergason@corwynn.com>
2022-06-09 19:45:31 -0700

    refactor loki module and place under observability

--- cmd/serve.go
+++ cmd/serve.go
@@ -24,11 +24,11 @@ import (
 	"time"
 
 	"github.com/penny-vault/pv-api/common"
-	"github.com/penny-vault/pv-api/common/loki"
 	"github.com/penny-vault/pv-api/data"
 	"github.com/penny-vault/pv-api/data/database"
 	"github.com/penny-vault/pv-api/jwks"
 	"github.com/penny-vault/pv-api/middleware"
+	"github.com/penny-vault/pv-api/observability/loki"
 	"github.com/penny-vault/pv-api/router"
 	"github.com/penny-vault/pv-api/strategies"
 	"github.com/penny-vault/pv-api/tradecron"
