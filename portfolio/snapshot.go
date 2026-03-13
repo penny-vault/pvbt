ee1e8613c1abd8fb5ccd6ef2dd75d90d26186c3e
Jeremy Fergason <jeremy@fergason.me>
2026-03-12 19:56:22 -0700

    feat(portfolio): add Metrics and AllMetadata to PortfolioSnapshot interface

---
+++ portfolio/snapshot.go
@@ -0,0 +1,69 @@
+// Copyright 2021-2026
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package portfolio
+
+import (
+	"time"
+
+	"github.com/penny-vault/pvbt/asset"
+)
+
+// TaxLot tracks the purchase date, quantity, and price of a position for
+// tax gain/loss calculations.
+type TaxLot struct {
+	Date  time.Time
+	Qty   float64
+	Price float64
+}
+
+// PortfolioSnapshot exposes the state needed to reconstruct a portfolio
+// from a previous run. Account implements this interface. The engine
+// uses it with WithPortfolioSnapshot to resume from saved state.
+type PortfolioSnapshot interface {
+	Cash() float64
+	Holdings(func(asset.Asset, float64))
+	Transactions() []Transaction
+	EquityCurve() []float64
+	EquityTimes() []time.Time
+	BenchmarkPrices() []float64
+	RiskFreePrices() []float64
+	TaxLots() map[asset.Asset][]TaxLot
+	Metrics() []MetricRow
+	AllMetadata() map[string]string
+}
+
+// WithPortfolioSnapshot returns an Option that restores an Account from
+// a previous snapshot. This is used by the engine to resume from saved state.
+func WithPortfolioSnapshot(snap PortfolioSnapshot) Option {
+	return func(a *Account) {
+		a.cash = snap.Cash()
+		snap.Holdings(func(ast asset.Asset, qty float64) {
+			a.holdings[ast] = qty
+		})
+		a.transactions = append(a.transactions, snap.Transactions()...)
+		a.equityCurve = append(a.equityCurve, snap.EquityCurve()...)
+		a.equityTimes = append(a.equityTimes, snap.EquityTimes()...)
+		a.benchmarkPrices = append(a.benchmarkPrices, snap.BenchmarkPrices()...)
+		a.riskFreePrices = append(a.riskFreePrices, snap.RiskFreePrices()...)
+		for ast, lots := range snap.TaxLots() {
+			a.taxLots[ast] = append(a.taxLots[ast], lots...)
+		}
+		a.metrics = append(a.metrics, snap.Metrics()...)
+		for k, v := range snap.AllMetadata() {
+			a.metadata[k] = v
+		}
+	}
+}
