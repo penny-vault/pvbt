57eda2e571366c6bf028b63786ceda9a71411acc
Jeremy Fergason <jeremy@fergason.me>
2026-03-14 13:21:55 -0700

    docs(universe): add package-level doc comment covering universe interface and membership

--- universe/universe.go
+++ universe/universe.go
@@ -13,7 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package universe defines collections of assets that strategies operate on.
 package universe
 
 import (
