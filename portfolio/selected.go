c52ac69c0f4e35e023253040d715d566095e7056
Jeremy Fergason <jeremy@fergason.me>
2026-03-14 00:01:33 -0700

    feat(portfolio): add Selected metric constant

---
+++ portfolio/selected.go
@@ -0,0 +1,23 @@
+// Copyright 2021-2026
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package portfolio
+
+import "github.com/penny-vault/pvbt/data"
+
+// Selected is the metric key used by Selectors to mark which assets
+// are chosen at each timestep. Values > 0 mean selected; 0 or NaN
+// means not selected. Supports fractional values for future selectors.
+const Selected data.Metric = "selected"
