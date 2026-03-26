203bbb8236e0674375b301ce63d7d26eb4636b83
Jeremy Fergason <jeremy@fergason.me>
2026-03-26 00:11:26 -0700

    feat(webull): add package scaffolding with doc, errors, and test suite (#18)

---
+++ broker/webull/exports_test.go
@@ -0,0 +1,8 @@
+package webull
+
+import (
+	"github.com/penny-vault/pvbt/broker"
+)
+
+// HTTPError is a type alias for broker.HTTPError.
+type HTTPError = broker.HTTPError
---
+++ broker/webull/webull_suite_test.go
@@ -0,0 +1,13 @@
+package webull_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestWebull(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Webull Suite")
+}
