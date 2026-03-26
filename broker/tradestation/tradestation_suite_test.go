d216a062b19654dc0358c377e85b2ba0af334369
Jeremy Fergason <jeremy@fergason.me>
2026-03-26 00:26:54 -0700

    fix(tradestation): redirect zerolog to GinkgoWriter in test suite (#19)

--- broker/tradestation/tradestation_suite_test.go
+++ broker/tradestation/tradestation_suite_test.go
@@ -5,9 +5,13 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/rs/zerolog/log"
 )
 
 func TestTradeStation(t *testing.T) {
+	// Send all zerolog output to GinkgoWriter so it only appears on test failure.
+	log.Logger = log.Output(GinkgoWriter)
+
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "TradeStation Suite")
 }
